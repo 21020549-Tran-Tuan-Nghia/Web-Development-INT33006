09adbfe281363de2e06836dbf311ad308da0a7ce
scallop200603 <tuannghia200603@gmail.com>
2023-09-23 16:14:05 +0700

    Finish w4

--- W4/tab/main.js
+++ W4/tab/main.js
@@ -1,25 +1,33 @@
-let link = document.getElementById('myframe');
-console.log(link.src);
+// document.addEventListener("DOMContentLoaded", function() {
+//     const items = document.querySelectorAll('.item')
+//     items.forEach((item) => {
+//         item.addEventListener('click', () => {
+//             items.forEach((item) => {
+//                 item.classList.remove('blue')
+//             })
+//             item.classList.add('blue')
+//         })
+//     })
+// })
 document.addEventListener("DOMContentLoaded", function() {
-    const items = document.querySelectorAll('.item')
+    const items = document.querySelectorAll('.item');
+    const iframe = document.getElementById('myframe');
+
+    const urls = {
+        'VnExpress': 'https://vnexpress.net/',
+        'Dantri': 'https://dantri.com.vn/',
+        'VOANews': 'https://www.voanews.com/',
+        'Zing': 'https://zingmp3.vn'
+    };
+
     items.forEach((item) => {
         item.addEventListener('click', () => {
-            switch (item.textContent) {
-                case 'Dantri':
-                    link.src = "https://dantri.com.vn/"
-                    break;
-            
-                default:
-                    break;
-            }
             items.forEach((item) => {
-                item.classList.remove('blue')
-            })
-            item.classList.add('blue')
-        })
-    })
-})
-
-// document.querySelector('.vnexpress').onclick = function() {
-//     document.getElementsByTagName('iframe').style.src = "https://dantri.com.vn/"
-// }
+                item.classList.remove('blue');
+            });
+            item.classList.add('blue');
+            const tabText = item.textContent.trim();
+            iframe.src = urls[tabText];
+        });
+    });
+});
